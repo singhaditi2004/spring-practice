79e0b0c728dda82fc1095038f16eb3babc8f6f3f
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-08-01 10:50:42 +0530

    Collection type

--- src/main/java/com/springcore/collections/Collections.java
+++ src/main/java/com/springcore/collections/Collections.java
@@ -8,42 +8,62 @@ import java.util.Set;
 public class Collections {
 	private String name;
 	private List<String> customers;
-    private Set<String> address;
-    private Map<String,Integer>stock;
-    private Properties prop;
-	public Collections(String name, List<String> customers, Set<String> address, Map<String, Integer> stock) {
+	private Set<String> address;
+	private Map<String, Integer> stock;
+	private Properties prop;
+
+	public Collections(String name, List<String> customers, Set<String> address, Map<String, Integer> stock,
+			Properties prop) {
 		super();
 		this.name = name;
 		this.customers = customers;
 		this.address = address;
 		this.stock = stock;
+		this.prop = prop;
 	}
+
 	public Collections() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public List<String> getCustomers() {
 		return customers;
 	}
+
 	public void setCustomers(List<String> customers) {
 		this.customers = customers;
 	}
+
 	public Set<String> getAddress() {
 		return address;
 	}
+
 	public void setAddress(Set<String> address) {
 		this.address = address;
 	}
+
 	public Map<String, Integer> getStock() {
 		return stock;
 	}
+
 	public void setStock(Map<String, Integer> stock) {
 		this.stock = stock;
+	}
+
+	public Properties getProp() {
+		return prop;
+	}
+
+	public void setProp(Properties prop) {
+		this.prop = prop;
 	}
 }
---
+++ src/main/java/com/springcore/collections/Main.java
@@ -0,0 +1,18 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/configuration.xml");
+         Collections col1=(Collections)context.getBean("rmp1");
+         System.out.println(col1.getName());
+         System.out.println(col1.getAddress());
+         System.out.println(col1.getCustomers());
+         System.out.println(col1.getStock());
+         System.out.println(col1.getProp());
+         
+	}
+}
